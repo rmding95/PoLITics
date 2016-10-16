d6290097e5b5d1d2f02d51d37377ef8a956b7b36
Richard Ding <richard.ding@alaskaair.com>
2016-10-15 22:46:33 -0700

    geocoding pls

--- TwitterScraper.py
+++ TwitterScraper.py
@@ -1,4 +1,6 @@
 import twitter
+import geocoder
+from Tweet import Tweet
 
 api = twitter.Api(consumer_key='rnBTENQ1GCJdZLVEuZheV6YJ6',
                   consumer_secret='b9g5TgNIXRKN7lwQxh5YcLk8AI59zQK3zzIAtAorspMHpUha3F',
@@ -8,8 +10,14 @@ hashtags = ['TrumpPence']
 tweets = []
 results = api.GetSearch(raw_query='f=tweets&vertical=default&q=%23TrumpPence&count=100')
 for result in results:
-    if (result.coordinates is not None) or (result.location is not None) or (result.place is not None) \
-            or (result.geo is not None) or (result.user.geo_enabled is True):
-        tweets.append(result)
-
+    if result.place is not None:
+        bounding_box = result.place.get('bounding_box').get('coordinates')[0]
+        tweet = Tweet((bounding_box[0][0] + bounding_box[1][0]) / 2, (bounding_box[1][1] + bounding_box[2][1]) / 2,
+                      'party', result.created_at)
+        tweets.append(tweet)
+    if (result.user.geo_enabled is True) and (len(result.user.location) > 0):
+        coords = geocoder.google(result.user.location)
+        if coords:
+            tweet = Tweet(coords.latlng[0], coords.latlng[1], 'party', result.created_at)
+            tweets.append(tweet)
 print(tweets)
