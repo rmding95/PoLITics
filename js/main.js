14a96472d6d5b95802a8afba96011989f77e47b3
Sam Wahbeh <stwahbeh@gmail.com>
2016-10-16 00:42:49 -0700

    added d3 files

--- js/main.js
+++ js/main.js
@@ -1,29 +1,70 @@
 var map;
+
 function initialize() {
-  map = new google.maps.Map(document.getElementById('map'), {
+  var mapOptions = {
     zoom: 2,
-    center: new google.maps.LatLng(2.8,-187.3),
+    center: { lat: -33.865427, lng: 151.196123 },
     mapTypeId: 'terrain'
-  });
+  };
+
+  map = new google.maps.Map(document.getElementById('map'),
+    mapOptions);
 
   // Create a <script> tag and set the USGS URL as the source.
   var script = document.createElement('script');
+
   // (In this example we use a locally stored copy instead.)
   // script.src = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp';
   script.src = 'https://developers.google.com/maps/documentation/javascript/tutorials/js/earthquake_GeoJSONP.js';
   document.getElementsByTagName('head')[0].appendChild(script);
+
+  map.data.setStyle(function (feature) {
+    var magnitude = feature.getProperty('mag');
+    return {
+      icon: getCircle(magnitude)
+      
+    };
+  });
 }
 
-// Loop through the results array and place a marker for each
-// set of coordinates.
-window.eqfeed_callback = function(results) {
-  for (var i = 0; i < results.features.length; i++) {
-    var coords = results.features[i].geometry.coordinates;
-    var latLng = new google.maps.LatLng(coords[1],coords[0]);
-    var marker = new google.maps.Marker({
-      position: latLng,
-      map: map
-    });
+function getCircle(magnitude) {
+   if (magnitude >= 4){
+  var circle = {
+    path: google.maps.SymbolPath.CIRCLE,
+    fillColor: 'red',
+    fillOpacity: .2,
+    scale: Math.pow(2, 3) / 2,
+    strokeColor: 'white',
+    strokeWeight: .5,
+  };
+  } else if (magnitude <= 3) {
+    var circle = {
+    path: google.maps.SymbolPath.CIRCLE,
+    fillColor: 'blue',
+    fillOpacity: .2,
+    scale: Math.pow(2, 3) / 2,
+    strokeColor: 'white',
+    strokeWeight: .5,
+  };
+  } else {
+   var circle = {
+    path: google.maps.SymbolPath.CIRCLE,
+    fillColor: 'green',
+    fillOpacity: .2,
+    scale: Math.pow(2, 3) / 2,
+    strokeColor: 'white',
+    strokeWeight: .5,
+  }; 
   }
+
+  return circle;
 }
+
+function eqfeed_callback(results) {
+  map.data.addGeoJson(results);
+
+  setTimeout(eqfeed_callback(null, results), 2000); 
+}
+
+
 
